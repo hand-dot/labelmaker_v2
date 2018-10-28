c8e07e1de6a060598c996033ecc9d100078ecab6
hand-dot <kyouhei.fukuda0729@gmail.com>
2018-10-29 04:54:06 +0900

    change desigin

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,22 +5,43 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import Notifications from '@material-ui/icons/Notifications';
+import Input from '@material-ui/core/Input';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import NativeSelect from '@material-ui/core/NativeSelect';
 import Typography from '@material-ui/core/Typography';
 
 const styles = {
+  white: {
+    color: '#fff',
+  },
   grow: {
     flexGrow: 1,
   },
 };
 
+
 function Header(props) {
-  const { classes } = props;
+  const {
+    template, templates, handleChangeTemplate, classes,
+  } = props;
   return (
     <AppBar position="fixed">
       <Toolbar>
         <Typography variant="h6" color="inherit" className={classes.grow}>
         レターパックラベルを一括作成！
         </Typography>
+        <FormControl>
+          <InputLabel classes={{ root: classes.white }} htmlFor="select-template-helper">テンプレート</InputLabel>
+          <NativeSelect
+            classes={{ root: classes.white }}
+            value={template}
+            onChange={handleChangeTemplate}
+            input={<Input name="age" id="select-template-helper" />}
+          >
+            {templates.map(_ => (<option key={_} value={_}>{_}</option>))}
+          </NativeSelect>
+        </FormControl>
         <IconButton color="inherit">
           <Notifications />
           <span style={{ position: 'absolute', left: 15, top: 15 }} id="changelog" />
@@ -31,6 +52,9 @@ function Header(props) {
 }
 
 Header.propTypes = {
+  template: PropTypes.string.isRequired,
+  templates: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+  handleChangeTemplate: PropTypes.func.isRequired,
   classes: PropTypes.object.isRequired, // eslint-disable-line
   theme: PropTypes.object.isRequired, // eslint-disable-line
 };
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,43 +1,27 @@
 import React, { Component } from 'react';
 import Handsontable from 'handsontable';
+import Grid from '@material-ui/core/Grid';
 import debounce from 'lodash.debounce';
 import 'handsontable/dist/handsontable.full.min.css';
 import Header from '../components/Header';
-import Controls from '../components/Controls';
 import TemplateEditor from '../components/TemplateEditor';
-import Modal from '../components/Modal';
-import { Title, Contents, Action } from '../components/ModalContents/PdfCreated';
-
 import '../styles/handsontable-custom.css';
 import '../styles/animation.css';
 import templates from '../templates';
-import util from '../utils';
 import pdfUtil from '../utils/pdf';
 
 const PDF_REFLESH_MS = 500;
+const windowSeparatorRatio = window.innerWidth * 0.2;
 const emptyIframe = new Blob(['<div>左のテーブルを記入して下さい。</div>'], { type: 'text/html' });
 
 // Hotのデータから全て空の行のデータを除去したものを返します。
 const getNotEmptyRowData = sourceData => sourceData.filter(data => Object.keys(data).some(key => data[key])); // eslint-disable-line 
-
-// 全角文字を半角にして、おところのカラムで改行の無い、長すぎる(26文字)文字列に改行を挿入する
-const formatData = datas => datas.map((data) => {
-  const clonedData = JSON.parse(JSON.stringify(data));
-  Object.keys(clonedData).forEach((key) => {
-    const text = util.zenkaku2hankaku(data[key]);
-    if (key === ('to_add' || 'from_add') && !/\n/g.test(data[key])) {
-      clonedData[key] = util.splitByLength(text, 26).join('\n');
-    }
-  });
-  return clonedData;
-});
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.hotInstance = null;
     this.state = {
-      isOpenModal: false,
       isTemplateEditor: false, // テンプレート開発者用。公開していません。
       selectedTemplate: 'letterpack',
     };
@@ -46,81 +30,81 @@ class App extends Component {
   componentDidMount() {
     const { selectedTemplate } = this.state;
     if (!this.hotDom) return;
-    const self = this;
     this.hotInstance = Handsontable(this.hotDom, {
       height: window.innerHeight - (this.hotContainer ? this.hotContainer.getBoundingClientRect().top : 0),
-      width: (window.innerWidth / 2),
+      width: (window.innerWidth / 2) + windowSeparatorRatio,
       rowHeaders: true,
-      stretchH: 'all',
       minRows: 50,
-      colWidths: Math.round(window.innerWidth / templates[selectedTemplate].columns.length) - 50,
+      colWidths: 200,
       columns: templates[selectedTemplate].columns,
       dataSchema: templates[selectedTemplate].dataSchema,
       afterChange: debounce(() => {
-        self.refleshPdf();
+        this.refleshPdf();
       }, PDF_REFLESH_MS),
     });
     this.iframe.src = URL.createObjectURL(emptyIframe);
-    this.forceUpdate(); // this.iframeを再計算させる
+    setTimeout(() => {
+      this.loadSampleData();
+    }, 4000); // FIXME 読み込みを待ってからじゃないとpdfmakeで落ちる
   }
 
-  handleOpenModal = () => {
-    this.setState({ isOpenModal: true });
-  };
-
-  handleCloseModal = () => {
-    this.setState({ isOpenModal: false });
-  };
+  handleChangeTemplate(e) {
+    if (!this.hotInstance) return;
+    const datas = getNotEmptyRowData(this.hotInstance.getSourceData());
+    if (datas.length !== 0 && !window.confirm('データがすでに入力されていますがテンプレートを変更しますか？')) return;
+    const selectedTemplate = e.target.value;
+    this.hotInstance.updateSettings({
+      columns: templates[selectedTemplate].columns,
+      dataSchema: templates[selectedTemplate].dataSchema,
+      data: [],
+    });
+    this.setState({ selectedTemplate });
+    setTimeout(() => { this.loadSampleData(); });
+  }
 
   async refleshPdf() {
     const { selectedTemplate } = this.state;
     const datas = getNotEmptyRowData(this.hotInstance.getSourceData());
-    const blob = await pdfUtil.getBlob(formatData(datas),
-      templates[selectedTemplate].image, templates[selectedTemplate].position);
+    const blob = await pdfUtil.getBlob(datas, templates[selectedTemplate].image, templates[selectedTemplate].position);
     this.iframe.src = URL.createObjectURL(blob);
   }
 
   loadSampleData() {
     const { selectedTemplate } = this.state;
-    if (!this.hotInstance) return;
-    const datas = getNotEmptyRowData(this.hotInstance.getSourceData());
-    if (datas.length !== 0 && !window.confirm('データがすでに入力されていますがサンプルを読み込みますか？')) return;
     const sampledata = JSON.parse(JSON.stringify(templates[selectedTemplate].sampledata));
     this.hotInstance.loadData(sampledata);
   }
 
   render() {
-    const { isTemplateEditor, isOpenModal, selectedTemplate } = this.state;
+    const { isTemplateEditor, selectedTemplate } = this.state;
     return (
       <>
-        <Header />
+        <Header
+          template={selectedTemplate}
+          templates={Object.keys(templates)}
+          handleChangeTemplate={this.handleChangeTemplate.bind(this)}
+        />
         <div style={{ marginBottom: 64 }} />
         {isTemplateEditor && <TemplateEditor />}
         {!isTemplateEditor && (
-        <>
-          <Controls loadSampleData={this.loadSampleData.bind(this)} />
-          <div style={{ display: 'flex' }}>
+        <Grid container justify="space-between">
+          <Grid item xs={6}>
             <div ref={(node) => { this.hotContainer = node; }}>
               <div ref={(node) => { this.hotDom = node; }} />
             </div>
+          </Grid>
+          <Grid item xs={6}>
             <iframe
+              style={{ position: 'fixed', right: 0, border: '1px solid #ccc' }}
               ref={(node) => { this.iframe = node; }}
               height={`${window.innerHeight - (this.iframe ? this.iframe.getBoundingClientRect().top + 5 : 0)}px`}
-              width={`${(window.innerWidth / 2)}px`}
+              width={`${(window.innerWidth / 2) - windowSeparatorRatio}px`}
               id="pdfIframe"
               title="PDF"
             />
-          </div>
-        </>
+          </Grid>
+        </Grid>
         )}
-        <Modal
-          open={isOpenModal}
-          onClose={this.handleCloseModal.bind(this)}
-          title={(<Title />)}
-          action={(<Action onClick={this.handleCloseModal} />)}
-        >
-          <Contents selectedTemplate={selectedTemplate} />
-        </Modal>
       </>
     );
   }
